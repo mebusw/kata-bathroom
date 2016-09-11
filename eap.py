fd8ae15948d932730580b6c776064a1e4a4f3f15
Jacky Shen <mebusw@163.com>
2016-09-11 21:58:16 +0800

    add tests

--- eap.py
+++ eap.py
@@ -30,9 +30,44 @@ rooms = [
   }
 ]
 
+only_ava_rooms = [
+  {
+     'room_id': 1,
+     'periods': [
+     ]
+  }, {
+     'room_id': 2,
+     'periods': [
+        {
+           'time': '18:00-19:00',
+           'status': 'available'
+        }
+     ]
+  }
+]
+
+no_ava_rooms = [
+  {
+     'room_id': 1,
+     'periods': [
+     ]
+  }
+]
+
 from pipe import *
 
 def ealiest_avaiable_period(room):
-  return {'room_id': room['room_id'], 'time': sorted(room['periods'] | where(lambda p: p['status'] == 'available'), key=lambda p: p['time'])[0]['time']}
+  s = sorted(room['periods'] | where(lambda p: p['status'] == 'available'), key=lambda p: p['time'])
+  return {'room_id': room['room_id'], 'time': s[0]['time'] if len(s) > 0 else 'N/A'}
 
-print sorted(rooms | select(ealiest_avaiable_period), key=lambda x: x['time'])[0]
+def most_eap(rooms):
+  s = sorted(rooms | select(ealiest_avaiable_period) | where(lambda r: r['time'] != 'N/A'), key=lambda x: x['time'])
+  return s[0] if len(s) > 0 else 'No Availiable Room'
+
+
+
+assert {'room_id': 1, 'time': '16:00-17:00'} == most_eap(rooms)
+
+assert {'room_id': 2, 'time': '18:00-19:00'} == most_eap(only_ava_rooms)
+
+assert 'No Availiable Room' == most_eap(no_ava_rooms)
